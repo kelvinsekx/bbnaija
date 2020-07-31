ccf35e31c177c30cdbc1c56cd8f84201b97e422f
kelvinsekx <utdkelvin@gmail.com>
2020-07-30 18:00:06 -1200

    fd

--- src/components/logo.js
+++ src/components/logo.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Logo(){
     return (
-        <span style={{color:'#098765', fontSize: "1.65em"}}>
+        <span style={{color:'#098765', fontSize: "1em"}}>
             <span className="binge">B</span>
             <span className="binge">B</span>NAIJA
             <span className="yellow">O</span>CTOPU
